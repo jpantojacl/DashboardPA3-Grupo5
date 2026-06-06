c414439fcb20a27197cb6362a7e507003928bf6e
jpantojacl <gerardopacla@gmail.com>
2026-06-07 09:51:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ st.markdown("""
 
     /* Reducir espacio SOLO entre Keywords y Carga de Datos */
     .reduce-space {
-        margin-top: -50px !important;    /
+        margin-top: -200px !important;    /
     }
 
     </style>
