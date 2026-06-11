d07b8039110f149d46f11499c95e094a98696cfe
jpantojacl <gerardopacla@gmail.com>
2026-06-11 13:32:16 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import re
+import collections
+
+# ==========================================
+# CONFIGURACIÓN DE LA PÁGINA
+# ==========================================
+st.set_page_config(
+    page_title="Dashboard - ML & Football Scouting",
+    page_icon="⚽",
+    layout="wide"
+)
+
+# Forzar estilo claro en Plotly
+px.defaults.template = "simple_white"
+px.defaults.color_discrete_sequence = ["#1D4ED8"]  # azul fuerte
+
+# ==========================================
+# ESTILOS
+# ==========================================
 st.markdown("""
     <style>
 
-    /* ======== TÍTULO PRINCIPAL ======== */
     .main-title { 
         font-size:30px !important; 
         font-weight: bold; 
@@ -10,15 +31,12 @@ st.markdown("""
         margin-bottom: 10px; 
     }
 
-    /* ======== SUBTÍTULO ======== */
     .subtitle { 
         font-size:16px !important; 
         color: #FFFFFF !important; 
         text-align: center; 
         margin-bottom: 30px; 
     }
-
-    /* ======== AJUSTES DEL SIDEBAR ======== */
 
     [data-testid="stSidebar"] * {
         font-size: 15px !important;
